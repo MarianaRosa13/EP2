12aba3d93b95c1b779864a9b09564fa111be5257
Mariana Rosa <marivillelarosa@gmail.com>
2024-04-22 11:01:07 -0300

    terceira versao

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from constants import *
-
+from functions import *
+import time
 
 print(PAISES)
 num_pais=int(input('Qual é o número da nação da sua frota?'))
@@ -12,10 +13,49 @@ for navio,quant in dicio.items():
     for i in range(quant):
         lista_frota.append(navio)
 
-for arma in lista_frota:
+posicao_letra=input('Informe a letra:')
+posicao_num=input('Informe o número:')
+posicao=posicao_letra + posicao_num
+
+
+
+
+#for arma in lista_frota:
     #aloca
+    # if arma=='cruzador':
+    #     print('▓'*2)
+    # if arma=='porta-avioes':
+    #     print('▓'*5)
+    # if arma=='couracado':
+    #     print('▓'*4)
+    # if arma=='destroyer':
+    #     print('▓'*3)
+    # if arma=='submarino':
+    #     print('▓'*2)
+    # if arma=='torpedeiro':
+    #     print('▓'*)????????
+
+print('Iniciando batalha naval!')
+time.sleep(1)
+i=5
+while i!=0:
+    print(i)
+    time.sleep(1)
+    i-=1
 
 
+#criar um dicionario com as posicoes em que há navioes depois de alocar
+#se a posicao estiver no dicionario: boomm, senao: agua 
 
 
-#quando ganhar: print('Você venceu!') print('Temos um novo cherife nos mares!')
+#venceu=foi_derrotado(mapa)
+
+# if venceu==True:
+#     print('Você venceu!')
+#     print('Temos um novo cherife nos mares!')
+#     print('Jogar novamente? [s/n]')
+
+# else:
+#     print('Você perdeu!')
+#     print('O computador ainda é o senhor dos mares!')
+#     print('Jogar novamente? [s/n]')
