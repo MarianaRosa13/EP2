093e86fa8d70dc5624cabddd54516c1ffcaa6f8f
Mariana Rosa <marivillelarosa@gmail.com>
2024-04-26 17:17:17 -0300

    alteracoes mapa

--- functions.py
+++ functions.py
@@ -1,9 +1,11 @@
+from constants import *
+
 def cria_mapa(n):
     matriz=[]
-    linha_nova=[]
-    while len(linha_nova)<n:
-        linha_nova.append(' ')
-    while len(matriz)<n:
+    for _ in range(n):
+        linha_nova=[]
+        for _ in range(n):
+            linha_nova.append('   ')
         matriz.append(linha_nova)
     return matriz
 
@@ -11,7 +13,7 @@ def foi_derrotado(matriz):
     for i in range(len(matriz)):
         linha=matriz[i]
         for j in range(len(linha)):
-            if linha[j]=='N':
+            if linha[j]==' N ':
                 return False
     return True
 
@@ -20,20 +22,20 @@ def posicao_suporta(mapa,num,num_linha,pos_letra,orient):
     if orient=='h':
         if len(linha)-pos_letra<num:
             return False
-        if linha[pos_letra]=='N':
+        if linha[pos_letra]==' N ':
             return False
         else:
             for i in range(0,num):
-                if mapa[num_linha][pos_letra+i]=='N':
+                if mapa[num_linha][pos_letra+i]==' N ':
                     return False
     elif orient=='v':
         if len(mapa)-num_linha<num:
             return False
-        if linha[pos_letra]=='N':
+        if linha[pos_letra]==' N ':
             return False
         else:
             for i in range(0,num):
-                if mapa[num_linha+i][pos_letra]=='N':
+                if mapa[num_linha+i][pos_letra]==' N ':
                     return False
     return True
 
@@ -49,7 +51,22 @@ def aloca_navios(mapa,lista):
             orientacao=random.choice(['h', 'v'])
         for i in range(0,quant):
             if orientacao=='h':
-                mapa[linha][coluna+i]='N'
+                mapa[linha][coluna+i]=' N '
             elif orientacao=='v':
-                mapa[linha+i][coluna]='N'            
+                mapa[linha+i][coluna]=' N '            
     return mapa
+
+
+def imprime_mapa(m):
+    dim = len(m)
+    letras=ALFABETO[:dim]
+    cabecalho=''
+    for i in letras:
+        cabecalho+=' '+ i + ' '
+    print(cabecalho)
+    for lin in range(dim):
+        linha = f'{lin+1} '
+        for col in range(dim):
+            linha += m[lin][col]
+        linha += f'{lin+1} '
+        print(linha)
--- main.py
+++ main.py
@@ -8,61 +8,106 @@ print(f'Você escolheu a nação {lista_paises[num_pais-1]}')
 n=int(input('Qual é o tamanho do seu tabuleiro?'))
 print('Agora é a sua vez de alocar seus navios de guerra!')
 
+mapa=cria_mapa(n)
+print(mapa)
+
 dicio=PAISES[lista_paises[num_pais-1]]
 lista_frota=[]
 for navio,quant in dicio.items():
     for i in range(quant):
         lista_frota.append(navio)
 
-
+cabecalho='  '
 letras=ALFABETO[:n]
-cabecalho=''
 for i in letras:
-    cabecalho+=' '+ i + ' '
+   cabecalho+='  '+ i + '  '
 
 grade=''
 i=0
 while i<=n:
-    grade+=NUMEROS[i] + '\n'     #TEM QUE ARRUMAR O 10!!!!!!!!
+    if i==NUMEROS[-2]:
+       grade+=NUMEROS[i]+NUMEROS[i+1]
+    else:
+        grade+=NUMEROS[i] + '\n'
     i+=1
-print(grade)
 
-posicao_letra=input('Informe a letra:').upper()
-while posicao_letra not in cabecalho:
-    print('Posição inválida')
-    posicao_letra=input('Informe a letra:').upper()
-posicao_num=input('Informe o número:')
-if posicao_num not in grade:
-    print('Posição inválida')
-    posicao_num=input('Informe o número:')
-orient=input('Informe a orientação: [v/h]')
-posicao_stg=posicao_letra + posicao_num
-print(posicao_stg)
-num_linha=int(posicao_num)
 
-for l in range(len(ALFABETO)):
-   if ALFABETO[l]==posicao_letra:
-      num_coluna=l
-print(num_coluna)
+# print(' '+ cabecalho + ' ')
+# for i in mapa:
+#    linha_branco=str(i)
+#    print(grade[i+1] + linha_branco + grade[i+1])  #TA DANDO PROBLEMA NO I+1!!!!!!!!!! not convert to list
+
+
+# posicao_letra=input('Informe a letra:').upper()
+# while posicao_letra not in cabecalho:
+#     print('Posição inválida')
+#     posicao_letra=input('Informe a letra:').upper()
+# posicao_num=input('Informe o número:')
+# if posicao_num not in grade:
+#     print('Posição inválida')
+#     posicao_num=input('Informe o número:')        COLOQUEI ESSE CODIGO DENTRO DO FOR NA LINHA 65!!!!!!!!!!!
+# orient=input('Informe a orientação: [v/h]')       TEM QUE TESTAR ELE NO FOR!!!!!!!!!!
+# posicao_stg=posicao_letra + posicao_num
+# print(posicao_stg)
+# num_linha=int(posicao_num)
+# for l in range(len(ALFABETO)):
+#    if ALFABETO[l]==posicao_letra:
+#       num_coluna=l
+# print(num_coluna)
 
 print('Ainda falta: '+', '.join(lista_frota))
 
-print(cabecalho) 
 
-mapa=cria_mapa(n)
 
-# for arma in lista_frota:
-#   if posicao_suporta(mapa,quant,posicao_num,posicao_letra,orient)==True:
-#     for pos in mapa:
-#       if pos=='N':
-#         pos='\u001b[33m▓'
+for arma in lista_frota:
+  for navio,quant in dicio.items():
+     print(lista_frota)
+     posicao_letra=input('Informe a letra:').upper()
+     while posicao_letra not in cabecalho:
+        print('Posição inválida')
+        posicao_letra=input('Informe a letra:').upper()
+     posicao_num=input('Informe o número:')
+     if posicao_num not in grade:
+        print('Posição inválida')
+        posicao_num=input('Informe o número:')
+     orient=input('Informe a orientação: [v/h]')
+     posicao_stg=posicao_letra + posicao_num
+     print(posicao_stg)
+     num_linha=int(posicao_num)
+     for l in range(len(ALFABETO)):
+        if ALFABETO[l]==posicao_letra:
+           num_coluna=l
+           print(num_coluna)
+     for navio,quant in dicio.items():
+        if posicao_suporta(mapa,quant,num_linha,num_coluna,orient)==True:
+           print('entrouuuuuuu')
+           imprime_mapa(mapa)
+           for i in range(0,quant):
+            if orient=='h':
+               mapa[num_linha-1][num_coluna+i]=' N '
+            elif orient=='v':
+               mapa[num_linha-1+i][num_coluna]=' N '
+            imprime_mapa(mapa)
+            #lista_frota.remove(navio)
+            print('Ainda falta: '+', '.join(lista_frota))
+        # else:
+        #    continue
+
+
+for pos in mapa:
+    if pos==' N ':
+        pos='\u001b[33m▓'
+            #print(pos)
+imprime_mapa(mapa)
+
+
 
 print('Iniciando batalha naval!')
 time.sleep(1)
 i=5
 while i!=0:
     print(i)
-    time.sleep(1)
+    #time.sleep(1)
     i-=1
 
 
@@ -75,7 +120,7 @@ for l in range(len(ALFABETO)):
 num_atq=int(input('Digite o número:'))
 posicao_atq=mapa[num_atq][coluna_atq]
 print(posicao_atq)
-if posicao_atq=='N':
+if posicao_atq==' N ':
   print(f'Jogador --->>>{posicao_atq}')
   print('BOOOMMM!!')
   posicao_atq='\u001b[31m▓'
@@ -83,7 +128,7 @@ else:
   print(f'Jogador --->>>{posicao_atq}')
   print('Água!')
   posicao_atq='\u001b[34m▓'
-print(mapa)
+imprime_mapa(mapa)
 #print(mapa_comp)
 
 
